2beeaaec274b1081f4667c74807db59e74803e8c
Heiman Chan <heimanchan0@gmail.com>
2019-05-14 19:33:02 -0700

    Add out of boundary logic in toggle

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
+const WIDTH = 7;
+const HEIGHT = 5;
+
 function toggleCell(x ,y) {
-  document.querySelector(`#cell-${x}-${y}`).classList.toggle("on");
+  if ((x >= 0 && x < WIDTH) && (y >= 0 && y < HEIGHT)) {
+    document.querySelector(`#cell-${x}-${y}`).classList.toggle("on");
+  }
 }
 
 function toggleCellAndNeighbors(x ,y) {
