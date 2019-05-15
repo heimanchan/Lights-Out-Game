e15af568de82c8eacee391ea361f25e2a8b18847
Heiman Chan <heimanchan0@gmail.com>
2019-05-14 19:23:25 -0700

    Add toggleCellAndNeighbors function

--- game.js
+++ game.js
@@ -1,3 +1,11 @@
-function toggleCell(y ,x) {
-  document.querySelector(`#cell-${y}-${x}`).classList.toggle("on")
+function toggleCell(x ,y) {
+  document.querySelector(`#cell-${x}-${y}`).classList.toggle("on");
+}
+
+function toggleCellAndNeighbors(x ,y) {
+  toggleCell(x, y);
+  toggleCell(x - 1, y);
+  toggleCell(x + 1, y);
+  toggleCell(x, y - 1);
+  toggleCell(x, y + 1);
 }
