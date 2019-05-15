a905552079bbbe5aa837a2a8b2cba729853fec6a
Heiman Chan <heimanchan0@gmail.com>
2019-05-14 20:03:26 -0700

    Implement click to toggle color

--- game.js
+++ game.js
@@ -1,22 +1,25 @@
 const WIDTH = 7;
 const HEIGHT = 5;
 
-function toggleCell(x ,y) {
+function toggleCell(y ,x) {
   if ((x >= 0 && x < WIDTH) && (y >= 0 && y < HEIGHT)) {
-    document.querySelector(`#cell-${x}-${y}`).classList.toggle("on");
+    document.querySelector(`#cell-${y}-${x}`).classList.toggle("on");
   }
 }
 
-function toggleCellAndNeighbors(x ,y) {
-  toggleCell(x, y);
-  toggleCell(x - 1, y);
-  toggleCell(x + 1, y);
-  toggleCell(x, y - 1);
-  toggleCell(x, y + 1);
+function toggleCellAndNeighbors(y ,x) {
+  toggleCell(y, x);
+  toggleCell(y - 1, x);
+  toggleCell(y + 1, x);
+  toggleCell(y, x - 1);
+  toggleCell(y, x + 1);
 }
 
 function handleClick() {
-  console.log("hello world");
+  // console.log(event.target.id);
+  let x, y;
+  let coordinates = event.target.id.match(/\d+/g);
+  toggleCellAndNeighbors(parseInt(coordinates[0]), parseInt(coordinates[1]));
 }
 
 document.querySelector(".game").addEventListener("click", handleClick);
